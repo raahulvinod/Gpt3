30c4eeabc0b725894f922f8b77b0195b316e40f6
Rahul vinod <rahulvinod135@gmail.com>
2023-04-24 01:21:16 +0530

    components and containers added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './App.css';
+
+import { Brand, CTA, Navbar } from './components';
+import {
+  Footer,
+  Blog,
+  Features,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from './containers';
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
